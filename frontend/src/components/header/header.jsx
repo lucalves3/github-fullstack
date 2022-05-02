59c2567db6ccaff9c774c132d536c69fcbd25080
lucalves3 <lucalves.rodrigues@gmail.com>
2022-05-01 21:35:24 -0300

    Remove useNavigation

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useNavigate, useLocation, Link } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
 import HeaderSTL from './headerSTL.styled';
 import githubIcon from '../../images/githubIcon.svg';
 
